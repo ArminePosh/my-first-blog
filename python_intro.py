182463dcd3b7bf58e07f5051ea7645f5fa9d7ca7
armine <armine.hakopyan@anvileight.com>
2019-11-30 15:29:05 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+a = [1, 2, 3, 4, 5, 6, 7]
+b = 0
+for nomer in a:
+    b = (b + nomer)
+    if nomer == 4:
+        break
+print (b)
+a = [1, 2, 3, 4, 5, 6, 7]
+b = 0
+for nomer in a:
+      b = (b + nomer)
+print (b)
